287983f79e2a681c2ae7c6f69667f985ece0c78d
slicg <slicgslicg@gmail.com>
2017-10-03 16:44:04 -0400

    class intro

---
+++ sample.cpp
@@ -0,0 +1,14 @@
+#include"sample.h"
+
+using namespace Exercises;
+
+int main()
+{
+Exercise ex;// Exercise object
+ex.x =3;
+ex.y ="Hello";
+
+ex.print();
+std::cout<<"Hello world\n";
+return 0;
+}
